f4e9018e2aae478769d861ff03ede7d3fbaa67d1
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-03-10 23:55:02 +0000

    changed to color detection

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ kernel = np.ones((5, 5), np.uint8)
 
 while vc.isOpened():
     ret, frame = vc.read()
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    thr_value, img_thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, kernel)
-    img_canny = cv2.Canny(img_close, 100, 200)
-    contours, hierarchy = cv2.findContours(img_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #thr_value, img_thresh = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # B, G, R
+    lower_green = np.array([0,100,0])
+    upper_green = np.array([0,255,0])
+    mask_blue = cv2.inRange(imghsv, lower_green, upper_green)
+    #img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, kernel)
+    #img_canny = cv2.Canny(img_close, 100, 200)
+    contours, hierarchy = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours,-1,(0,255,0),1)
 
 
