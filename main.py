eb7d218b89e76268cbec16ccf69ca55af87a16be
Nuno Ferreira <nunomiguelferreira5@gmail.com>
2023-03-14 16:39:34 +0000

    draw contours

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ images_folder = r'/home/nuno/Documents/lego_assembly_assistant/lego_images/'
 lego_image = 'y'
 
 
-vc = cv2.VideoCapture(1)
+# vc = cv2.VideoCapture(1)
 kernel = np.ones((5, 5), np.uint8)
 
-img = cv2.imread(images_folder + lego_image + '.jpg')
+img = cv2.imread(images_folder + lego_image + '.jpeg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thr_value, img_thresh = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY)  # add this: cv2.THRESH_OTSU
+thr_value, img_thresh = cv2.threshold(img_gray, 160, 255, cv2.THRESH_BINARY)  # add this: cv2.THRESH_OTSU
 contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+for i, c in enumerate(contours):
+    cv2.drawContours(img, contours, i, (0, 255, 0), 1)
 
 # while vc.isOpened():
 #     ret, frame = vc.read()
@@ -35,5 +37,10 @@ contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPR
 #     if key == 27:
 #         break
 
-cv2.destroyWindow('stream')
-vc.release()
+cv2.namedWindow('picture', cv2.WINDOW_NORMAL)
+cv2.imshow('picture',img)
+key = cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# cv2.destroyWindow('stream')
+# vc.release()
