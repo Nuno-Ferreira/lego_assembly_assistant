bb120a9e2089dd413c200b03b1760577238c974f
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-03-24 17:15:34 +0000

    changed the values and combined the 2 red masks to fully get the red colour

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 # IMAGE FOLDER AND IMAGE NAME
 # NEED TO CHANGE THE PATH TO THE FOLDER WHERE THE IMAGES ARE TO BE ABLE TO WORK ON ALL OS
 #images_folder = r'/home/nuno/Documents/lego_assembly_assistant/lego_images/' # USED FOR LINUX
-images_folder = r'D:/GitHub/lego_assembly_assistant/lego_images/' # USED FOR WINDOWS
+images_folder = r'./lego_images/' # USED FOR WINDOWS
 lego_image = 'all'
 
 # SETTING UP THE KERNEL AND IMAGE
@@ -14,22 +14,21 @@ img = cv2.imread(images_folder + lego_image + '.jpeg')
 
 # GREEN COLOR RANGES
 lower_green = np.array([50,50,50])
-upper_green = np.array([80,180,180])
+upper_green = np.array([80,255,255])
 
 # YELLOW COLOR RANGES
 lower_yellow = np.array([20,100,100])
-upper_yellow = np.array([50,180,180])
+upper_yellow = np.array([50,255,255])
 
 # RED COLOR RANGES
-# need to combine these 2 ranges to detect the full range of red 
-lower_red1 = np.array([0,0,0])
-upper_red1 = np.array([10,180,180])
-lower_red2 = np.array([170,0,0]) 
-upper_red2 = np.array([180,180,180])
+lower_red1 = np.array([0,50,50])
+upper_red1 = np.array([5,255,255])
+lower_red2 = np.array([170,50,50]) 
+upper_red2 = np.array([180,255,255])
 
 # BLUE COLOR RANGES
 lower_blue = np.array([90,100,100])
-upper_blue = np.array([160,180,180])
+upper_blue = np.array([160,255,255])
 
 # GREEN COLOR DETECTION
 imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -42,7 +41,9 @@ green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
 
 # RED COLOR DETECTION
 mask_red = cv2.inRange(imghsv, lower_red1, upper_red1)
-img_red_mask = cv2.bitwise_and(img, img, mask=mask_red) # need to change this to bitwise_or to combine the 2 ranges
+mask_red2 = cv2.inRange(imghsv, lower_red2, upper_red2)
+combined_mask = cv2.bitwise_or(mask_red, mask_red2) # COMBINE THE TWO MASKS DUE TO HOW HSV WORKS
+img_red_mask = cv2.bitwise_and(img, img, mask=combined_mask)
 red_mask = cv2.morphologyEx(img_red_mask, cv2.MORPH_CLOSE, kernel)
 red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
 # contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
