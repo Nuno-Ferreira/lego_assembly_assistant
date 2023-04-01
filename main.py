edbc7d10d836f07406e394a90168e27e708065f6
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-04-01 14:52:35 +0100

    detected the outermost contours to then be able to calculate the size of the pieces relative to size of main board

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # IMAGE FOLDER AND IMAGE NAME
 images_folder = r'./lego_images/' # USED FOR ALL OS
-lego_image = 'y'
+lego_image = 'board2'
 
 # SETTING UP THE KERNEL AND IMAGE
 kernel = np.ones((5, 5), np.uint8)
@@ -30,6 +30,7 @@ upper_blue = np.array([160,255,255])
 
 
 #------------------------------------------------- YELLOW COLOR DETECTION -----------------------------------------------#
+
 imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 mask_yellow = cv2.inRange(imghsv, lower_yellow, upper_yellow)
 img_yellow_mask = cv2.bitwise_and(img, img, mask=mask_yellow)
@@ -43,23 +44,17 @@ thr_value, img_thresh = cv2.threshold(img_yellow, 100, 200, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(img_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 yellow_output = cv2.drawContours(img, contours, -1, (0, 255, 255), 2)
 
-# CIRCLE DETECTION
-circles = cv2.HoughCircles(img_yellow, cv2.HOUGH_GRADIENT, 1, 10, param1=30, param2=50, minRadius=0, maxRadius=1000)
-
-if circles is not None:
-    detected_circles = np.uint16(np.around(circles))
-    for (x, y, r) in detected_circles[0, :]:
-        cv2.circle(img, (x, y), r, (0, 255, 0), 2)
-
 # COLOUR IDENTIFICATION
 for i, c in enumerate(contours):
     M = cv2.moments(c)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
+    area = cv2.contourArea(c)
     cv2.putText(yellow_output, 'YELLOW', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
 
 
 #------------------------------------------------- GREEN COLOR DETECTION -----------------------------------------------#
+
 mask_green = cv2.inRange(imghsv, lower_green, upper_green)
 img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
 green_mask = cv2.morphologyEx(img_green_mask, cv2.MORPH_CLOSE, kernel)
@@ -74,10 +69,12 @@ for i, c in enumerate(contours):
     M = cv2.moments(c)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
+    area = cv2.contourArea(c)
     cv2.putText(green_output, 'GREEN', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
 
 
 #------------------------------------------------ RED COLOR DETECTION   -----------------------------------------------#
+
 mask_red = cv2.inRange(imghsv, lower_red1, upper_red1)
 mask_red2 = cv2.inRange(imghsv, lower_red2, upper_red2)
 combined_mask = cv2.bitwise_or(mask_red, mask_red2) # COMBINE THE TWO MASKS TO DETECT RED
@@ -94,10 +91,12 @@ for i, c in enumerate(contours):
     M = cv2.moments(c)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
+    area = cv2.contourArea(c)
     cv2.putText(red_output, 'RED', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
 
 
 #----------------------------------------------- BLUE COLOR DETECTION   -----------------------------------------------#
+
 mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
 img_blue_mask = cv2.bitwise_and(img, img, mask=mask_blue)
 blue_mask = cv2.morphologyEx(img_blue_mask, cv2.MORPH_CLOSE, kernel)
@@ -112,13 +111,15 @@ for i, c in enumerate(contours):
     M = cv2.moments(c)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
+    area = cv2.contourArea(c)
     cv2.putText(blue_output, 'BLUE', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
 
 
 #---------------------------------------------- IMAGE PROCESSING    --------------------------------------------------#
+
 img = cv2.imread(images_folder + lego_image + '.jpeg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_blur = cv2.blur(img_gray, (3, 3))
+img_blur = cv2.blur(img_gray, (5, 5))
 img_erode = cv2.erode(img_blur, kernel, iterations=1)
 img_dilate = cv2.dilate(img_erode, kernel, iterations=1)
 thr_value, img_thresh = cv2.threshold(img_blur, 100, 200, cv2.THRESH_BINARY)  # add this: cv2.THRESH_OTSU
@@ -126,14 +127,29 @@ img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)
 contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 # DISPLAYING CONTOURS
-# for i, c in enumerate(contours):
-#     cv2.drawContours(img, contours, i, (0, 255, 0), 2)
+for i, c in enumerate(contours):
+    x,y,w,h = cv2.boundingRect(c)
+    if hierarchy[0,i,3] == -1 and len(contours[i]) >= 100:
+        cv2.rectangle(img, (x,y),( x+w,y+h), (0, 255, 0), 2)
+        # cv2.drawContours(img, contours, i, (0, 255, 0), 2)
 
 
-# DISPLAYING IMAGES
-# cv2.imshow('picture', img)
+
+# REPLACE THIS BY COMPUTING THE SIZE OF THE MAIN BOARD AND THEN COMPARE IT TO THE SIZE OF THE OTHER PIECES TO GET 4X4/3X3/2X2
+# # CIRCLE DETECTION
+# circles = cv2.HoughCircles(img_yellow, cv2.HOUGH_GRADIENT, 1, 10, param1=30, param2=50, minRadius=0, maxRadius=1000)
+
+# if circles is not None:
+#     detected_circles = np.uint16(np.around(circles))
+#     for (x, y, r) in detected_circles[0, :]:
+#         cv2.circle(img, (x, y), r, (0, 255, 0), 2)
+
+
+
+#------------------------------------- DISPLAYING IMAGES    ----------------------------------------------------------#
+
+cv2.imshow('picture', img)
 cv2.imshow('red mask', red_output)
-# cv2.imshow('blurred', blurred)
 # cv2.imshow('blue mask', blue_output)
 # cv2.imshow('green mask', green_output)
 
