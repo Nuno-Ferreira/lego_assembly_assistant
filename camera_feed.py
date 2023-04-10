38800e26ada950938c4a17d66386be5edc73030e
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-04-10 17:29:12 +0100

    added the main board detection

--- camera_feed.py
+++ camera_feed.py
@@ -6,10 +6,41 @@ vc = cv2.VideoCapture(url)
 
 kernel = np.ones((5, 5), np.uint8)
 
+
+# GREEN COLOR RANGES
+lower_green = np.array([50,50,50])
+upper_green = np.array([80,255,255])
+
+
+# TELL USER TO PLACE THE MAIN GREEN BOARD IN THE CENTER OF THE CAMERA FEED 
+# USE A GREEN MASK TO DETECT THE BIGGEST CONTOUR (MAIN BOARD) -- CONSISTENT LIGHTING AND CAMERA ANGLE
+# USE ONE OF THE FRAMES TO CALCULATE THE RATIOS BETWEEN PIXELS AND CM
+# USE THAT RATIO TO CALCULATE THE SIZE OF THE LEGO PIECES
+# COMPUTE THE AREA OF THE BOARD AND USE IT TO DETECT THE PIECES
+# MAIN BOARD = 8x16 
+
+
+# ADD PIXEL TO CM RATIO
+pixel_cm_ratio = 1
+
+def get_main_board():
+    mask_green = cv2.inRange(imghsv, lower_green, upper_green)
+    img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
+    green_mask = cv2.morphologyEx(img_green_mask, cv2.MORPH_CLOSE, kernel)
+    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
+
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE GREEN MASK
+    img_green = cv2.cvtColor(green_mask, cv2.COLOR_BGR2GRAY)
+    thr_value, img_thresh = cv2.threshold(img_green, 100, 200, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(img_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    green_output = cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+
+
+
 while vc.isOpened():
     ret, frame = vc.read()
 
-
+    # IMAGE PROCESSING
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.blur(img_gray, (5, 5))
     img_erode = cv2.erode(img_blur, kernel, iterations=2)
@@ -17,6 +48,14 @@ while vc.isOpened():
     thr_value, img_thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # add this: cv2.THRESH_OTSU / OTHER VALUES 50 - 80
     img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)
     contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    # GETTING THE BIGGEST CONTOUR (MAIN BOARD)
+    rect = cv2.minAreaRect()
+    (x, y), (w, h), angle = rect
+
+    # GETTING THE WIDTH AND HEIGHT OF THE MAIN BOARD
+    board_width = w / pixel_cm_ratio
+    board_height = h / pixel_cm_ratio
 
     # DISPLAYING CONTOURS
     for i, c in enumerate(contours):
@@ -30,6 +69,18 @@ while vc.isOpened():
             # cv2.rectangle(img, (x,y),( x+w,y+h), (0, 255, 0), 2) # USED TO DRAW RECTANGLES AROUND THE PIECES
             cv2.drawContours(frame, contours, i, (0, 255, 0), 2) # USED TO DRAW CONTOURS AROUND THE PIECES
 
+
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
+
+# cv2.destroyWindow('stream')
+# vc.release()
+
+# UNUSED CODE
 
     # #img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -47,13 +98,3 @@ while vc.isOpened():
     # key = cv2.waitKey(1)
     # if key == 27:
     #     break
-
-    if frame is not None:
-        cv2.imshow("Frame", frame)
-    q = cv2.waitKey(1)
-    if q==ord("q"):
-        break
-cv2.destroyAllWindows()
-
-# cv2.destroyWindow('stream')
-# vc.release()
