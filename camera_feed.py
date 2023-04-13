cb3501fe2d4fb41f0ebd6b25230798607b82d5b1
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-04-13 18:21:12 +0100

    commented image processing code out

--- camera_feed.py
+++ camera_feed.py
@@ -130,25 +130,25 @@ while vc.isOpened():
     imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # IMAGE PROCESSING
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.blur(img_gray, (5, 5))
-    img_erode = cv2.erode(img_blur, kernel, iterations=2)
-    img_dilate = cv2.dilate(img_erode, kernel, iterations=2)
-    thr_value, img_thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # add this: cv2.THRESH_OTSU / OTHER VALUES 50 - 80
-    img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)
-    contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # img_blur = cv2.blur(img_gray, (5, 5))
+    # img_erode = cv2.erode(img_blur, kernel, iterations=2)
+    # img_dilate = cv2.dilate(img_erode, kernel, iterations=2)
+    # thr_value, img_thresh = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # add this: cv2.THRESH_OTSU / OTHER VALUES 50 - 80
+    # img_close = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)
+    # contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    # DISPLAYING CONTOURS
-    for i, c in enumerate(contours):
-        x,y,w,h = cv2.boundingRect(c)
-        area = cv2.contourArea(c)
-        length = len(contours[i])
-        if hierarchy[0,i,3] == -1 and len(contours[i]) >= 200:
-            area = cv2.contourArea(c)
-            print('area: ', area)
-            print('length: ', length)
-            # cv2.rectangle(img, (x,y),( x+w,y+h), (0, 255, 0), 2) # USED TO DRAW RECTANGLES AROUND THE PIECES
-            cv2.drawContours(frame, contours, i, (0, 255, 0), 2) # USED TO DRAW CONTOURS AROUND THE PIECES
+    # # DISPLAYING CONTOURS
+    # for i, c in enumerate(contours):
+    #     x,y,w,h = cv2.boundingRect(c)
+    #     area = cv2.contourArea(c)
+    #     length = len(contours[i])
+    #     if hierarchy[0,i,3] == -1 and len(contours[i]) >= 200:
+    #         area = cv2.contourArea(c)
+    #         print('area: ', area)
+    #         print('length: ', length)
+    #         # cv2.rectangle(img, (x,y),( x+w,y+h), (0, 255, 0), 2) # USED TO DRAW RECTANGLES AROUND THE PIECES
+    #         cv2.drawContours(frame, contours, i, (0, 255, 0), 2) # USED TO DRAW CONTOURS AROUND THE PIECES
 
 # INSTRUCT THE USER TO PLACE THE MAIN BOARD IN THE CENTER OF THE CAMERA FEED -- maybe use a button input to confirm it's been placed
 # PROCESS THE IMAGE TO DETECT THE MAIN BOARD
