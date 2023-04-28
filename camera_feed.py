fa2887336d9dd0dcb9a6ea07f9e6b0db0c5e6e6b
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2023-04-28 13:57:49 +0100

    modified the colour detection so that its not looping inside the function

--- camera_feed.py
+++ camera_feed.py
@@ -9,8 +9,8 @@ print("Starting program...")
 #--------------------------------- VARIABLES ---------------------------#
 url = "http://192.168.1.65:8080/video" # USING THE PHONE AS A WEBCAM
 vc = cv2.VideoCapture(1)
-ret, frame = vc.read()
-imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+# ret, frame = vc.read()
+# imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 # SETTING UP THE KERNEL
 kernel = np.ones((10, 10), np.uint8)
@@ -34,16 +34,16 @@ lower_yellow = np.array([20,100,100])
 upper_yellow = np.array([50,255,255])
 
 # DECLARING THE HEIGHT AND WIDTH RATIOS SO THEY CAN BE USED IN THE OTHER FUNCTIONS
-width_ratio = 0
-height_ratio = 0
-green_width_studs = 0
-green_height_studs = 0
-red_width_studs = 0
-red_height_studs = 0
-blue_width_studs = 0
-blue_height_studs = 0
-yellow_width_studs = 0
-yellow_height_studs = 0
+# width_ratio = 0
+# height_ratio = 0
+# green_width_studs = 0
+# green_height_studs = 0
+# red_width_studs = 0
+# red_height_studs = 0
+# blue_width_studs = 0
+# blue_height_studs = 0
+# yellow_width_studs = 0
+# yellow_height_studs = 0
 
 contours_queue = queue.Queue()
 c_queue_lock = threading.Lock()
@@ -65,162 +65,157 @@ def get_main_board(imghsv, img, kernel, lower_green, upper_green):
     
     # DECLARING THE HEIGHT AND WIDTH RATIOS SO THEY CAN BE USED IN THE OTHER FUNCTIONS
     global height_ratio, width_ratio
-    while True:
-        mask_green = cv2.inRange(imghsv, lower_green, upper_green)
-        img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
-        green_mask = cv2.dilate(img_green_mask, kernel, iterations=2)
-        green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_CLOSE, kernel)
-        green_mask = cv2.erode(green_mask, kernel, iterations=1)
-        # green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
+    mask_green = cv2.inRange(imghsv, lower_green, upper_green)
+    img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
+    green_mask = cv2.dilate(img_green_mask, kernel, iterations=2)
+    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_CLOSE, kernel)
+    green_mask = cv2.erode(green_mask, kernel, iterations=1)
+    # green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
 
-        # FOR DETECTING AND DRAWING THE CONTOURS OF THE GREEN MASK
-        img_green = cv2.cvtColor(green_mask, cv2.COLOR_BGR2GRAY)
-        thr_value, img_thresh = cv2.threshold(img_green, 100, 255, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        for c in contours:
-            # FIND THE MINIMUM AREA RECTANGLE OF THE CONTOUR
-            rect = cv2.minAreaRect(c) 
-            (x, y), (w, h), angle = rect
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE GREEN MASK
+    img_green = cv2.cvtColor(green_mask, cv2.COLOR_BGR2GRAY)
+    thr_value, img_thresh = cv2.threshold(img_green, 100, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    for c in contours:
+        # FIND THE MINIMUM AREA RECTANGLE OF THE CONTOUR
+        rect = cv2.minAreaRect(c) 
+        (x, y), (w, h), angle = rect
 
-            # MAKE SURE THE WIDTH IS ALWAYS GREATER THAN THE HEIGHT
-            if h > w:
-                h, w = w, h
+        # MAKE SURE THE WIDTH IS ALWAYS GREATER THAN THE HEIGHT
+        if h > w:
+            h, w = w, h
 
-            # IF THE WIDTH AND HEIGHT ARE GREATER THAN 50, THEN USE THOSE MEASUREMENTS FOR THE RATIOS
-            if h and w > 50:
-                # CALCULATE THE HEIGHT AND WIDTH RATIOS BASED ON THE HOW MANY STUDS THE MAIN BOARD HAS
-                height_ratio = h / 8
-                width_ratio = w / 16
+        # IF THE WIDTH AND HEIGHT ARE GREATER THAN 50, THEN USE THOSE MEASUREMENTS FOR THE RATIOS
+        if h and w > 50:
+            # CALCULATE THE HEIGHT AND WIDTH RATIOS BASED ON THE HOW MANY STUDS THE MAIN BOARD HAS
+            height_ratio = h / 8
+            width_ratio = w / 16
 
-                board_output = cv2.drawContours(img, c, -1, (0, 255, 0), 4)
-                with c_queue_lock:
-                    contours_queue.put(img, c)
+            board_output = cv2.drawContours(img, c, -1, (0, 255, 0), 4)
+            with c_queue_lock:
+                contours_queue.put(img, c)
 
 
 
 def green_detection(imghsv, img, kernel, lower_green, upper_green, width_ratio, height_ratio):
     global green_width_studs, green_height_studs
-    while True:
-        mask_green = cv2.inRange(imghsv, lower_green, upper_green)
-        img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
-        green_mask = cv2.morphologyEx(img_green_mask, cv2.MORPH_CLOSE, kernel)
-        green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
+    mask_green = cv2.inRange(imghsv, lower_green, upper_green)
+    img_green_mask = cv2.bitwise_and(img, img, mask=mask_green)
+    green_mask = cv2.morphologyEx(img_green_mask, cv2.MORPH_CLOSE, kernel)
+    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
 
-        # FOR DETECTING AND DRAWING THE CONTOURS OF THE RED MASK
-        img_green = cv2.cvtColor(green_mask, cv2.COLOR_BGR2GRAY)
-        # thr_value, img_thresh = cv2.threshold(img_red, 100, 200, cv2.THRESH_BINARY)
-        contours, hierarchy= cv2.findContours(img_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE RED MASK
+    img_green = cv2.cvtColor(green_mask, cv2.COLOR_BGR2GRAY)
+    # thr_value, img_thresh = cv2.threshold(img_red, 100, 200, cv2.THRESH_BINARY)
+    contours, hierarchy= cv2.findContours(img_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        for c in contours:
+    for c in contours:
 
-            rect = cv2.minAreaRect(c) 
-            (x, y), (w, h), angle = rect
+        rect = cv2.minAreaRect(c) 
+        (x, y), (w, h), angle = rect
 
-            if h > w:
-                h, w = w, h
+        if h > w:
+            h, w = w, h
 
-            green_width_studs = w / width_ratio
-            green_height_studs = h / height_ratio
+        green_width_studs = w / width_ratio
+        green_height_studs = h / height_ratio
 
-            if h and w > 50:
-                green_output = cv2.drawContours(img, c, -1, (0, 0, 255), 4)
-                #contours_queue.put(green_output)
+        if h and w > 50:
+            green_output = cv2.drawContours(img, c, -1, (0, 0, 255), 4)
+            #contours_queue.put(green_output)
 
 
 
 def red_detection(imghsv, img, kernel, lower_red1, upper_red1, lower_red2, upper_red2, width_ratio, height_ratio):
     global red_width_studs, red_height_studs
-    while True:
-        mask_red = cv2.inRange(imghsv, lower_red1, upper_red1)
-        mask_red2 = cv2.inRange(imghsv, lower_red2, upper_red2)
-        combined_mask = cv2.bitwise_or(mask_red, mask_red2) # COMBINE THE TWO MASKS TO DETECT RED
-        img_red_mask = cv2.bitwise_and(img, img, mask=combined_mask)
-        red_mask = cv2.morphologyEx(img_red_mask, cv2.MORPH_CLOSE, kernel)
-        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+    mask_red = cv2.inRange(imghsv, lower_red1, upper_red1)
+    mask_red2 = cv2.inRange(imghsv, lower_red2, upper_red2)
+    combined_mask = cv2.bitwise_or(mask_red, mask_red2) # COMBINE THE TWO MASKS TO DETECT RED
+    img_red_mask = cv2.bitwise_and(img, img, mask=combined_mask)
+    red_mask = cv2.morphologyEx(img_red_mask, cv2.MORPH_CLOSE, kernel)
+    red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
 
-        # FOR DETECTING AND DRAWING THE CONTOURS OF THE RED MASK
-        img_red = cv2.cvtColor(red_mask, cv2.COLOR_BGR2GRAY)
-        # thr_value, img_thresh = cv2.threshold(img_red, 100, 200, cv2.THRESH_BINARY)
-        contours, hierarchy= cv2.findContours(img_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE RED MASK
+    img_red = cv2.cvtColor(red_mask, cv2.COLOR_BGR2GRAY)
+    # thr_value, img_thresh = cv2.threshold(img_red, 100, 200, cv2.THRESH_BINARY)
+    contours, hierarchy= cv2.findContours(img_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        for c in contours:
+    for c in contours:
 
-            rect = cv2.minAreaRect(c) 
-            (x, y), (w, h), angle = rect
+        rect = cv2.minAreaRect(c) 
+        (x, y), (w, h), angle = rect
 
-            if h > w:
-                h, w = w, h
+        if h > w:
+            h, w = w, h
 
-            red_width_studs = w / width_ratio
-            red_height_studs = h / height_ratio
+        red_width_studs = w / width_ratio
+        red_height_studs = h / height_ratio
 
-            if h and w > 50:
-                red_output = cv2.drawContours(img, c, -1, (0, 0, 255), 4)
-                with c_queue_lock:
-                    contours_queue.put(img, c)
+        if h and w > 50:
+            red_output = cv2.drawContours(img, c, -1, (0, 0, 255), 4)
+            with c_queue_lock:
+                contours_queue.put(img, c)
 
 
 
 def blue_detection(imghsv, img, kernel, lower_blue, upper_blue, width_ratio, height_ratio):
     global blue_width_studs, blue_height_studs
-    while True:
-        mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
-        img_blue_mask = cv2.bitwise_and(img, img, mask=mask_blue)
-        blue_mask = cv2.morphologyEx(img_blue_mask, cv2.MORPH_CLOSE, kernel)
-        blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
+    mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
+    img_blue_mask = cv2.bitwise_and(img, img, mask=mask_blue)
+    blue_mask = cv2.morphologyEx(img_blue_mask, cv2.MORPH_CLOSE, kernel)
+    blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
 
-        # FOR DETECTING AND DRAWING THE CONTOURS OF THE BLUE MASK
-        img_blue = cv2.cvtColor(blue_mask, cv2.COLOR_BGR2GRAY)
-        # thr_value, img_thresh = cv2.threshold(img_blue, 100, 200, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(img_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE BLUE MASK
+    img_blue = cv2.cvtColor(blue_mask, cv2.COLOR_BGR2GRAY)
+    # thr_value, img_thresh = cv2.threshold(img_blue, 100, 200, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(img_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        for c in contours:
+    for c in contours:
 
-            rect = cv2.minAreaRect(c) 
-            (x, y), (w, h), angle = rect
+        rect = cv2.minAreaRect(c) 
+        (x, y), (w, h), angle = rect
 
-            if h > w:
-                h, w = w, h
+        if h > w:
+            h, w = w, h
 
-            blue_width_studs = w / width_ratio
-            blue_height_studs = h / height_ratio
+        blue_width_studs = w / width_ratio
+        blue_height_studs = h / height_ratio
 
-            if h and w > 50:
-                blue_output = cv2.drawContours(img, c, -1, (255, 0, 0), 4)
-                with c_queue_lock:
-                    contours_queue.put(img, c)
+        if h and w > 50:
+            blue_output = cv2.drawContours(img, c, -1, (255, 0, 0), 4)
+            with c_queue_lock:
+                contours_queue.put(img, c)
 
 
 
 def yellow_detection(imghsv, img, kernel, lower_yellow, upper_yellow, width_ratio, height_ratio):
     global yellow_width_studs, yellow_height_studs
-    while True:
-        mask_yellow = cv2.inRange(imghsv, lower_yellow, upper_yellow)
-        img_yellow_mask = cv2.bitwise_and(img, img, mask=mask_yellow)
-        yellow_mask = cv2.morphologyEx(img_yellow_mask, cv2.MORPH_CLOSE, kernel)
-        yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel)
+    mask_yellow = cv2.inRange(imghsv, lower_yellow, upper_yellow)
+    img_yellow_mask = cv2.bitwise_and(img, img, mask=mask_yellow)
+    yellow_mask = cv2.morphologyEx(img_yellow_mask, cv2.MORPH_CLOSE, kernel)
+    yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel)
 
-        # FOR DETECTING AND DRAWING THE CONTOURS OF THE YELLOW MASK
-        img_yellow = cv2.cvtColor(yellow_mask, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.medianBlur(img_yellow, 25)
-        thr_value, img_thresh = cv2.threshold(img_yellow, 100, 200, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(img_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # FOR DETECTING AND DRAWING THE CONTOURS OF THE YELLOW MASK
+    img_yellow = cv2.cvtColor(yellow_mask, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.medianBlur(img_yellow, 25)
+    thr_value, img_thresh = cv2.threshold(img_yellow, 100, 200, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(img_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        for c in contours:
-            
-            rect = cv2.minAreaRect(c) 
-            (x, y), (w, h), angle = rect
+    for c in contours:
+        
+        rect = cv2.minAreaRect(c) 
+        (x, y), (w, h), angle = rect
 
-            if h > w:
-                h, w = w, h
+        if h > w:
+            h, w = w, h
 
-            yellow_width_studs = w / width_ratio
-            yellow_height_studs = h / height_ratio
+        yellow_width_studs = w / width_ratio
+        yellow_height_studs = h / height_ratio
 
-            if h and w > 50:
-                yellow_output = cv2.drawContours(img, c, -1, (0, 255, 255), 4)
-                with c_queue_lock:
-                    contours_queue.put(img, c)
+        if h and w > 50:
+            yellow_output = cv2.drawContours(img, c, -1, (0, 255, 255), 4)
+            with c_queue_lock:
+                contours_queue.put(img, c)
 
 
 
@@ -275,6 +270,8 @@ print('Starting the while loop...')
 counter = 0
 board_counter = 0
 ui_counter = 0
+board_first_iteration = True
+ui_first_iteration = True
 
 while vc.isOpened():
     # READ THE FRAME AND CONVERT IT TO HSV
@@ -285,8 +282,8 @@ while vc.isOpened():
     counter += 1
     
     if board_counter == 0:
-        print('Place the main green board in the center of the camera feed and press Enter to continue')
-        if cv2.waitKey(1) == 13:
+        print('Place the main green board in the center of the camera feed and press Enter to continue') # NEED TO MAKE SURE THIS DOESN'T PRINT OVER AND OVER AGAIN ADD SOMETHING LIKE IF BOARD_COUNTER == 100
+        if cv2.waitKey(100) == 13: #THIS IS NOT WORKING SO NEED TO FIX IT
             break
         get_main_board(imghsv, frame, kernel, lower_green, upper_green)
         # print('Make sure that the whole main board is selected in the image and then press Enter to continue') # MAYBE DELETE THIS AS IT CAN BE REDUNDANT
